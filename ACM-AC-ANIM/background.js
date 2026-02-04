15cb020c6f009af241920baa0f2680e74295af36
ResLianko <1985746979@qq.com>
2026-02-04 19:43:24 +0800

    Comment out video URL listener in background.js
    Commented out the message listener and added a note.

--- ACM-AC-ANIM/background.js
+++ ACM-AC-ANIM/background.js
@@ -1,7 +1,11 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getVideoUrl") {
-        const videoUrl = chrome.runtime.getURL('assets/accepted.mp4');
-        sendResponse({ videoUrl });
-    }
-    return false;
-});
+// // background.js
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (request.action === "getVideoUrl") {
+//         const videoUrl = chrome.runtime.getURL('assets/accepted.mp4');
+//         sendResponse({ videoUrl });
+//     }
+//     return true;
+// });
+
+// upd: 清空了 background.js
